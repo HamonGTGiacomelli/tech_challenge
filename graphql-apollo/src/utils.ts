d5e498fe920336572b69cddd61f1e8c199a4d02f
Hamon Giacomelli <hamongtgiacomelli@hotmail.com>
2021-11-25 12:13:43 -0300

    add dotenv to application

--- graphql-apollo/src/utils.ts
+++ graphql-apollo/src/utils.ts
@@ -1,8 +1,10 @@
 import AWS from "aws-sdk";
 
+const {S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_PORT, S3_HOST} = process.env;
+
 export const s3 = new AWS.S3({
   s3ForcePathStyle: true,
-  accessKeyId: "S3RVER",
-  secretAccessKey: "S3RVER",
-  endpoint: new AWS.Endpoint("http://localhost:4569"),
+  accessKeyId: S3_ACCESS_KEY_ID,
+  secretAccessKey: S3_SECRET_ACCESS_KEY,
+  endpoint: new AWS.Endpoint(`http://${S3_HOST}:${S3_PORT}`),
 });
